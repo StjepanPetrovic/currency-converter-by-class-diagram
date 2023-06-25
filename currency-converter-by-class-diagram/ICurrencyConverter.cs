80ea9dc8d3cdafe35732e8e0fb0a3aa2a53c652d
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 15:28:42 +0200

    Add interface ICurrencyConverter

---
+++ currency-converter-by-class-diagram/ICurrencyConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_converter_by_class_diagram
+{
+    internal interface ICurrencyConverter
+    {
+        double Convert(Currency fromCurrency, Currency toCurrency, double amount);
+    }
+}
