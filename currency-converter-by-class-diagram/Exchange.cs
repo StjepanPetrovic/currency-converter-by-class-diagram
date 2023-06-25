001a95beeae6bac6dfc0bc1badab158b9f7ae9c5
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 15:52:39 +0200

    Add class Exchange class

---
+++ currency-converter-by-class-diagram/Exchange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_converter_by_class_diagram
+{
+    internal class Exchange
+    {
+        public ICurrencyConverter GetConverter(Currency fromCurrency, Currency toCurrency)
+        {
+            if (fromCurrency.Type == CurrencyType.Fiat && toCurrency.Type == CurrencyType.Fiat)
+            {
+                FiatConverter fiatConverter = new FiatConverter();
+
+                return fiatConverter;
+            }
+            else if (fromCurrency.Type == CurrencyType.Fiat && toCurrency.Type == CurrencyType.Fiat)
+            {
+                CryptoConverter cryptoConverter = new CryptoConverter();
+
+                return cryptoConverter;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public double Convert(Currency fromCurrency, Currency toCurrency, double amount)
+        {
+            ICurrencyConverter converter = GetConverter(fromCurrency, toCurrency);
+
+            return converter.Convert(fromCurrency, toCurrency, amount);
+        }
+    }
+}
