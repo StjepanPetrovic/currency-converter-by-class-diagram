3b42bfdf3a25b5d5cf5d042193c226a05bb446c1
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 15:34:40 +0200

    Add class FiatConverter which implement interface ICurrencyConverter

---
+++ currency-converter-by-class-diagram/FiatConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_converter_by_class_diagram
+{
+    internal class FiatConverter : ICurrencyConverter
+    {
+        public double Convert(Currency fromCurrency, Currency toCurrency, double amount)
+        {
+            return (fromCurrency.Rate / toCurrency.Rate) * amount * 0.98;
+        }
+    }
+}
