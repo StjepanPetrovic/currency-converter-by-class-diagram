649ffa73f7dda6914fc96d503be878b442257f63
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 15:26:17 +0200

    Add class Currency with enumeration CurrencyType

---
+++ currency-converter-by-class-diagram/Currency.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace currency_converter_by_class_diagram
+{
+    public enum CurrencyType
+    {
+        Fiat,
+        Crypto,
+    }
+
+    public class Currency
+    {
+        public string Name { get; set; }
+        public CurrencyType Type { get; set; }
+        public double Rate { get; set; }
+
+        public Currency(string name, CurrencyType type, double rate)
+        {
+            Name = name;
+            Type = type;
+            Rate = rate;
+        }
+    }
+}
